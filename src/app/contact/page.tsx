bbf5075922fff573607c69b00d3b4fc9ce4cece0
Laura Petit <lau.petit0@gmail.com>
2024-07-05 16:17:03 +0200

    initial commit

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+
+const About = () => {
+  return (
+    <div>
+      <Head>
+        <title>Contact</title>
+        <meta name="contact" content="contact" />
+      </Head>
+      <main>
+        <h1>Contact</h1>
+        <p>Nous sommes un fleuriste passionné par la création de magnifiques arrangements floraux.</p>
+      </main>
+    </div>
+  );
+};
+
+export default About;
